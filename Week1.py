6d89bbcdc03248f16337bd4d64d108ae4d83cf28
gtrejosd <45042368+gtrejosd@users.noreply.github.com>
2018-11-15 11:33:20 -0600

    Add files via upload

---
+++ Week1.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# First week of the Data Science Captsone
+
+# In[1]:
+
+
+import pandas as pd 
+
+
+# In[3]:
+
+
+import numpy as np 
+
+
+# In[ ]:
+
+
+print("Hello Capstone Project Course!")
+
